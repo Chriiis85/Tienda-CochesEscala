44e05852134fdee4e0e505276ee8e8325ab9fd7e
Christian Moreno Diaz <krizmoreno85@gmail.com>
2024-02-12 16:38:46 +0100

    Estilobase

--- index.php
+++ index.php
@@ -1,129 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Index - CModel Cars</title>
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
-      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-      crossorigin="anonymous"
-    />
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main Page - CModels Scale</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <script
-      src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
-      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
-      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-      crossorigin="anonymous"
-    ></script>
-  </head>
-  <body>
-    <!-- Section: Design Block -->
-    <section class="text-center" style="background: whitesmoke; height: 100vh;">
-      <!-- Background image -->
-      <div
-        class="p-5 bg-image"
-        style="
-          background-image: url(Images/Banner.png);
-          background-size: cover;
-          background-repeat: no-repeat;
-          height: 32vh;
-        "
-      ></div>
-      <!-- Background image -->
+</head>
+<body>
+    <header>
+        <section class="title">
+            <h1>CMODEL SCALE CARS</h1>
+        </section>
+        <section class="bar">
+            <div class="group">
+                <svg viewBox="0 0 24 24" aria-hidden="true" class="icon">
+                  <g>
+                    <path
+                      d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"
+                    ></path>
+                  </g>
+                </svg>
+                <input class="input" type="search" placeholder="Buscar Producto" />
+              </div>
+        </section>
+        <section class="botones">
+            <button class="buttonlogin" role="button"><a href="login.php">Iniciar Sesión</a></button>
+            <button class="buttonlogin" role="button">Crear Cuenta</button>
+            <button data-quantity="0" class="btn-cart">
+                <svg class="icon-cart" viewBox="0 0 24.38 30.52" height="30.52" width="24.38" xmlns="http://www.w3.org/2000/svg">
+                  <path transform="translate(-3.62 -0.85)" d="M28,27.3,26.24,7.51a.75.75,0,0,0-.76-.69h-3.7a6,6,0,0,0-12,0H6.13a.76.76,0,0,0-.76.69L3.62,27.3v.07a4.29,4.29,0,0,0,4.52,4H23.48a4.29,4.29,0,0,0,4.52-4ZM15.81,2.37a4.47,4.47,0,0,1,4.46,4.45H11.35a4.47,4.47,0,0,1,4.46-4.45Zm7.67,27.48H8.13a2.79,2.79,0,0,1-3-2.45L6.83,8.34h3V11a.76.76,0,0,0,1.52,0V8.34h8.92V11a.76.76,0,0,0,1.52,0V8.34h3L26.48,27.4a2.79,2.79,0,0,1-3,2.44Zm0,0"></path>
+                </svg>
+                <span class="quantity"></span>
+              </button>
 
-      <div
-        class="card mx-4 mx-md-5 shadow-5-strong"
-        style="
-          margin-top: -100px;
-          background: hsla(0, 0%, 100%, 0.8);
-          backdrop-filter: blur(30px);
-        "
-      >
-        <div class="card-body py-5 px-md-5">
-          <div class="row d-flex justify-content-center">
-            <div class="col-lg-8">
-              <h2 class="fw-bold mb-5">Iniciar Sesión</h2>
-              <form>
-                <!-- 2 column grid layout with text inputs for the first and last names -->
+        </section>
+    </header>
 
-                <!-- Email input -->
-                <div class="form-outline mb-4">
-                  <label class="form-label" for="form3Example3"
-                    >Nombre de Usuario</label
-                  >
-                  <input
-                    type="text"
-                    id="username"
-                    class="form-control"
-                    required
-                  />
-                </div>
-
-                <!-- Password input -->
-                <div class="form-outline mb-4">
-                  <label class="form-label" for="form3Example4"
-                    >Contraseña</label
-                  >
-                  <input
-                    type="password"
-                    id="password"
-                    class="form-control"
-                    required
-                  />
-                </div>
-
-                <!-- Submit button -->
-                <button type="submit" id="iniciarSesion" class="btn btn-primary btn-block mb-4">
-                  Iniciar Sesión
-                </button>
-              </form>
+    <section class="body">
+        <article class="car-presentation">
+            <div class="image">            
+                <img src="Imagenes/AMR23/AMR23-Imagen1.jpg" alt="Imagen coche AMR23 Fernando Alonso">
             </div>
-          </div>
-        </div>
-      </div>
+            <div class="description">
+                <h1>AMR23 - Aston Martin Fernando Alonso</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, sint praesentium fuga impedit nisi reiciendis similique nobis ad, ipsam omnis quos accusamus soluta velit aut reprehenderit repellat. Laboriosam, sunt dolorem.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, sint praesentium fuga impedit nisi reiciendis similique nobis ad, ipsam omnis quos accusamus soluta velit aut reprehenderit repellat. Laboriosam, sunt dolorem.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, sint praesentium fuga impedit nisi reiciendis similique nobis ad, ipsam omnis quos accusamus soluta velit aut reprehenderit repellat. Laboriosam, sunt dolorem.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, sint praesentium fuga impedit nisi reiciendis similique nobis ad, ipsam omnis quos accusamus soluta velit aut reprehenderit repellat. Laboriosam, sunt dolorem.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, sint praesentium fuga impedit nisi reiciendis similique nobis ad, ipsam omnis quos accusamus soluta velit aut reprehenderit repellat. Laboriosam, sunt dolorem.</p>
+            </div>
+        </article>
+        <hr>
+        <article class="car-presentation">
+            <div class="description">
+                <h1>C42 - Alfa Romeo Sauber Valtteri Bottas</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, sint praesentium fuga impedit nisi reiciendis similique nobis ad, ipsam omnis quos accusamus soluta velit aut reprehenderit repellat. Laboriosam, sunt dolorem.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, sint praesentium fuga impedit nisi reiciendis similique nobis ad, ipsam omnis quos accusamus soluta velit aut reprehenderit repellat. Laboriosam, sunt dolorem.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, sint praesentium fuga impedit nisi reiciendis similique nobis ad, ipsam omnis quos accusamus soluta velit aut reprehenderit repellat. Laboriosam, sunt dolorem.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, sint praesentium fuga impedit nisi reiciendis similique nobis ad, ipsam omnis quos accusamus soluta velit aut reprehenderit repellat. Laboriosam, sunt dolorem.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, sint praesentium fuga impedit nisi reiciendis similique nobis ad, ipsam omnis quos accusamus soluta velit aut reprehenderit repellat. Laboriosam, sunt dolorem.</p>
+            </div>
+            <div class="image">            
+                <img src="Imagenes/C42/C42-Imagen1.jpg" alt="Imagen coche AMR23 Fernando Alonso">
+            </div>
+        </article>
+        <hr>
+        <article class="car-presentation">
+            <div class="image">            
+                <img src="Imagenes/W14/W14-imagen1.jpg" alt="Imagen coche AMR23 Fernando Alonso">
+            </div>
+            <div class="description">
+                <h1>W14 - Mercedes AMG Petronas Lewis Hamilton</h1>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, sint praesentium fuga impedit nisi reiciendis similique nobis ad, ipsam omnis quos accusamus soluta velit aut reprehenderit repellat. Laboriosam, sunt dolorem.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, sint praesentium fuga impedit nisi reiciendis similique nobis ad, ipsam omnis quos accusamus soluta velit aut reprehenderit repellat. Laboriosam, sunt dolorem.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, sint praesentium fuga impedit nisi reiciendis similique nobis ad, ipsam omnis quos accusamus soluta velit aut reprehenderit repellat. Laboriosam, sunt dolorem.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, sint praesentium fuga impedit nisi reiciendis similique nobis ad, ipsam omnis quos accusamus soluta velit aut reprehenderit repellat. Laboriosam, sunt dolorem.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, sint praesentium fuga impedit nisi reiciendis similique nobis ad, ipsam omnis quos accusamus soluta velit aut reprehenderit repellat. Laboriosam, sunt dolorem.</p>
+            </div>
+        </article>
     </section>
-    <!-- Section: Design Block -->
-  </body>
-  <script>
-  let botonIniciarSesion = document.getElementById("iniciarSesion");
-  let username;
-  let password;
 
-  function validarUsuario(){
-    username = document.getElementById("username").value;
-    password = document.getElementById("password").value;
+    <footer></footer>
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-      if(this.readyState == 4 && this.status == 200){
-        if(this.responseText==1){
-          alert("¡Bienvenido, "+username+" !!");
-          location.href ="paginaprincipal.php";
-        }else{
-          alert("Nombre de usuario o contraseña incorrectos");
-          location.href ="index.php";
-        }
-      }
-    }
-    xhttp.open ("POST", "validar_usuario.php", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send("username=" + username + "&password=" + password);
-  }
+</body>
 
-  botonIniciarSesion.addEventListener("click", function(){
-    validarUsuario();
-  });
 
-</script>
 </html>
---
+++ login.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Index - CModel Cars</title>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
+
+    <script
+      src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+    <!-- Section: Design Block -->
+    <section class="text-center" style="background: #252525; height: 100vh;">
+      <!-- Background image -->
+      <div
+        class="p-5 bg-image"
+        style="
+          background-image: url(Images/Banner.png);
+          background-size: cover;
+          background-repeat: no-repeat;
+          height: 32vh;
+        "
+      ></div>
+
+      <div
+        class="card mx-4 mx-md-5 shadow-5-strong"
+        style="
+          margin-top: -100px;
+          background: hsla(0, 0%, 100%, 0.8);
+          backdrop-filter: blur(30px);
+          height:50vh;
+        "
+      >
+        <div class="card-body py-5 px-md-5">
+          <div class="row d-flex justify-content-center">
+            <div class="col-lg-8">
+              <h2 class="fw-bold mb-5">Iniciar Sesión</h2>
+              <form>
+
+                <!-- Username input -->
+                <div class="form-outline mb-4">
+                  <label class="form-label" for="form3Example3"
+                    >Nombre de Usuario</label
+                  >
+                  <input
+                    type="text"
+                    id="username"
+                    class="form-control"
+                    required
+                  />
+                </div>
+
+                <!-- Password input -->
+                <div class="form-outline mb-4">
+                  <label class="form-label" for="form3Example4"
+                    >Contraseña</label
+                  >
+                  <input
+                    type="password"
+                    id="password"
+                    class="form-control"
+                    required
+                  />
+                </div>
+
+                <!-- Submit button -->
+                <button type="submit" id="iniciarSesion" class="btn btn-primary btn-block mb-4">
+                  Iniciar Sesión
+                </button>
+                <!-- Submit button -->
+                <button id="volver" class="btn btn-primary btn-block mb-4">
+                  Volver Atrás
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+  <script>
+  let botonIniciarSesion = document.getElementById("iniciarSesion");
+  let botonAtras = document.getElementById("volver");
+  let username;
+  let password;
+
+  function validarUsuario(){
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+      if(this.readyState == 4 && this.status == 200){
+        if(this.responseText==1){
+          alert("¡Bienvenido, "+username+"!!");
+          location.href ="index.php";
+        }else{
+          alert("Nombre de usuario o contraseña incorrectos");
+          location.href ="login.php";
+        }
+      }
+    }
+    xhttp.open ("POST", "validar_usuario.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("username=" + username + "&password=" + password);
+  }
+
+  botonIniciarSesion.addEventListener("click", function(){
+    validarUsuario();
+  });
+
+  botonAtras.addEventListener("click", function(){
+    location.href ="index.php";
+  });
+
+
+</script>
+</html>
